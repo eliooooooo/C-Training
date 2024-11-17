c31c2951feb1bcb448fb7ad51e594d4e323ab66c
eliooooooo <eliott.burkle@gmail.com>
2024-11-17 15:38:44 +0100

    Feat: scanf exercice

--- Scanf/README.md
+++ Scanf/README.md
@@ -1,3 +1,5 @@
 Ton programme devra, grâce à une boucle, récupérer 5 entrée de l'utilisateur avec scanf et stocker ça dans un tableau
+
 Et seulement après les 5 entrées il va les afficher toutes en même temps
+
 Pour les entrées utilisateur, pas besoin de faire des listes dynamiques, tu peux juste partir du principe que ça ne dépasse pas 100 caractères
--- Scanf/main.c
+++ Scanf/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define nbInputs 5
+
+void getInputs()
+{
+
+    void* inputs[nbInputs];
+    int len_inputs = nbInputs;
+
+    printf("Enter %d input: ", nbInputs);
+    for (size_t i = 0; i < nbInputs; i++)
+    {   
+        char *pi = malloc(100 * sizeof(char));
+        scanf("%s", pi);
+        inputs[i] = (void*)pi;
+        printf("Input: %s\n", pi);
+        // printf("Adress: %p\n", inputs[i]);
+    }
+
+    puts("\n--------------\n");
+
+    for (size_t i = 0; i < len_inputs; i++)
+    {
+        char* po = inputs[i];
+        printf("Value n°%ld : %s\n", i+1, po);
+        free(po);
+    }
+}
 
 int main()
 {
-    printf("test\n");
+    getInputs();
     return 0;
 }
