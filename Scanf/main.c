d781aae43f88ac7ddeffb2ec0bcecf45ae1d910a
eliooooooo <eliott.burkle@gmail.com>
2024-11-17 16:53:46 +0100

    Feat: add main arguments

--- Scanf/main.c
+++ Scanf/main.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define nbInputs 5
-
-void getInputs()
+void getInputs( int nb_inputs )
 {
 
-    void* inputs[nbInputs];
-    int len_inputs = nbInputs;
+    char** inputs = malloc(nb_inputs*sizeof(char*));
 
-    printf("Enter %d input: ", nbInputs);
-    for (size_t i = 0; i < nbInputs; i++)
+    printf("Enter %d input: ", nb_inputs);
+    for (size_t i = 0; i < nb_inputs; i++)
     {   
         char *pi = malloc(100 * sizeof(char));
         scanf("%s", pi);
-        inputs[i] = (void*)pi;
+        inputs[i] = (char*)pi;
         printf("Input: %s\n", pi);
         // printf("Adress: %p\n", inputs[i]);
     }
 
     puts("\n--------------\n");
 
-    for (size_t i = 0; i < len_inputs; i++)
+    for (size_t i = 0; i < nb_inputs; i++)
     {
         char* po = inputs[i];
         printf("Value n°%ld : %s\n", i+1, po);
         free(po);
     }
+
+    free(inputs);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-    getInputs();
+    if (argc < 2)
+    {
+        fprintf(stderr, "Arguments error: <nb_inputs> is missing");
+        return 1;
+    }
+    
+    int nb = atoi(argv[1]);
+    getInputs(nb);
     return 0;
 }
